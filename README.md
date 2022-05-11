1f5e388d1231c62f70f9bd32c23ffdc8d1627ed9
poldiee <nessidave@gmail.com>
2022-05-11 18:08:41 +0300

    categorization of posts functionality creation

---
+++ README.md
@@ -0,0 +1,67 @@
+# pitch-world
+## Author
+
+[David Njagi](https://github.com/poldiee)
+
+# Description
+a flask application that allows users to post one minute pitches and also allows other users who have signed up to comment on pitch. It also allows a person to signup to be able to access the functionalities of the appl
+
+## Live Demo
+
+## Screenshot
+
+<img src="" width="700px" height="300px">
+
+## User Story
+
+* Comment on pitch posted py other uses.
+* See the pitches posted by other uses.
+* Vote on pitch they have viwed
+* Register to log in to the application
+* View different categories pitches.
+* Submit a pitch depending on specific category.
+
+
+## Development Installation
+To get the code..
+
+1. Cloning the repository:
+  ```bash
+  https://github.com/poldiee/pitch-site.git
+  ```
+2. Move to the folder and install requirements
+  ```bash
+  cd pitch-site
+  pip install -r requirements.txt
+  ```
+3. Exporting Configurations
+  ```bash
+  export SQLALCHEMY_DATABASE_URI=postgresql+psycopg2://{User Name}:{password}@localhost/{database name}
+  ```
+4. Running the application
+  ```bash
+  python3.8 manage.py server
+  ```
+5. Testing the application
+  ```bash
+  python3.8 manage.py test
+  ```
+Open the application on your browser `127.0.0.1:5000`.
+
+
+## Technology used
+
+* [Python3.6](https://www.python.org/)
+* [Flask](http://flask.pocoo.org/)
+* 
+
+
+## Known Bugs
+* 
+
+## Contact Information 
+any question or contributions, please email @[nessidave@gmail.com]
+
+## License
+* *MIT License:*
+* Copyright (c) 2022 **poldiee**
--- app/main/views.py
+++ app/main/views.py
@@ -8,10 +8,12 @@ from .. import db,photos
 @main.route('/')
 def index():
     pitches = Pitch.query.all()
-    job = Pitch.query.filter_by(category = 'job')   
-    return render_template('index.html', job = job, pitches=pitches)
+    job = Pitch.query.filter_by(category = 'Job').all() 
+    event = Pitch.query.filter_by(category = 'Events').all()
+    advertisement = Pitch.query.filter_by(category = 'Advertisement').all()
+    return render_template('index.html', job = job,event = event, pitches = pitches,advertisement= advertisement)
 
-@main.route('/new_pitch', methods = ['POST','GET'])
+@main.route('/create_new', methods = ['POST','GET'])
 @login_required
 def new_pitch():
     form = PitchForm()
@@ -20,20 +22,27 @@ def new_pitch():
         post = form.post.data
         category = form.category.data
         user_id = current_user
-        new_pitch = Pitch(post=post,user_id=current_user._get_current_object().id,category=category,title=title)
-        new_pitch.save_p()
+        new_pitch_object = Pitch(post=post,user_id=current_user._get_current_object().id,category=category,title=title)
+        new_pitch_object.save_p()
         return redirect(url_for('main.index'))
     return render_template('create_pitch.html', form = form)
 
-@main.route('/new_comment/<int:pitch_id>')
+@main.route('/comment/<int:pitch_id>', methods = ['POST','GET'])
 @login_required
 def comment(pitch_id):
     form = CommentForm()
+    pitch = Pitch.query.get(pitch_id)
+    all_comments = Comment.query.filter_by(pitch_id = pitch_id).all()
     if form.validate_on_submit():
-        comment = form.comment.data
-        post_id = Pitch.query.get(pitch_id)
+        comment = form.comment.data 
+        pitch_id = pitch_id
         user_id = current_user._get_current_object().id
         new_comment = Comment(comment = comment,user_id = user_id,pitch_id = pitch_id)
+
+        new_comment.save_c()
+        return redirect(url_for('.comment', pitch_id = pitch_id))
+    return render_template('comment.html', form =form, pitch = pitch,all_comments=all_comments)
+
 
 @main.route('/user/<name>')
 def profile(name):
