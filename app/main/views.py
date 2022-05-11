0a746867fe0c0f2c3a005da49785e361d7989c27
poldiee <nessidave@gmail.com>
2022-05-11 15:41:23 +0300

    forms model and signup template addition

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,8 @@
 from flask import render_template, redirect, url_for
 from . import main
+from flask_login import login_required
 
 @main.route('/')
+@login_required
 def index():
     return render_template('index.html')
--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+
+    # print(SQLALCHEMY_DATABASE_URI)
+    SQLALCHEMY_TRACK_MODIFICATIONS=True
+    SECRET_KEY=os.environ.get('SECRET_KEY')
     Debug=True 
