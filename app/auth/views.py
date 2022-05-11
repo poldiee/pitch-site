8135fdeeba800a760d52e1acac748adae7fc81ac
poldiee <nessidave@gmail.com>
2022-05-11 16:45:40 +0300

    login for registered user via login view template

--- app/auth/views.py
+++ app/auth/views.py
@@ -16,6 +16,12 @@ def login():
         flash('Invalid username or Password')
     return render_template('auth/login.html', loginform = form)
 
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
+
 @auth.route('/signup', methods = ["GET","POST"])
 def signup():
     form = RegForm()
